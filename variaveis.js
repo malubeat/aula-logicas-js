c7c1e232fef9f145b60d47289d38ad40d56e4dfd
ANA CAROLINA QUIRINO SILVA FRANCISCO <ana_c_francisco@estudante.sesisenai.org.br>
2025-04-22 19:05:21 -0300

    js

---
+++ variaveis.js
@@ -0,0 +1,20 @@
+//declaração de variável com var
+
+/*var nome = "Luciano"
+
+var nome = "João"*/
+
+//declaração de variável com let
+
+// let nome = "Luciano"
+
+// nome = "João"
+
+//variável declarada com const
+
+const nome = "Luciano"
+
+nome = "João"
+
+
+console.log(nome)
